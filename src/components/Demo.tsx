09c0dfeced0243b00530cb00f5e152998284c525
Mohammed Abed <63855373+mabedd@users.noreply.github.com>
2024-06-18 16:23:32 +0300

    Initial Commiy

---
+++ src/components/Demo.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
+type Props = {};
+
+const Demo = (props: Props) => {
+  return <div>Demo</div>;
+};
+
+export default Demo;
